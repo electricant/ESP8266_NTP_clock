f9ae52f52e92257654609bf95bc38388593594ec
electricant <electricant@anche.no>
2021-04-02 11:03:24 +0200

    Fix deprecated BUILTIN_LED issue

--- esp_clock_firmware/d1_gpio_utils.ino
+++ esp_clock_firmware/d1_gpio_utils.ino
@@ -3,7 +3,7 @@
 void gpios_init()
 {
   // initialize onboard LED as output
-  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   // set BACKLIGHT_BUTTON pin as input pullup to detect button press
   pinMode(BACKLIGHT_BUTTON, INPUT_PULLUP);
   // also set the backlight pin as output
@@ -15,17 +15,17 @@ void gpios_init()
 void builtin_LED_set_brightness(uint8_t percent)
 {
   int pwm_val = 1023 - (1023*percent)/100;
-  analogWrite(BUILTIN_LED, pwm_val);
+  analogWrite(LED_BUILTIN, pwm_val);
 }
 
 inline void builtin_LED_on()
 {
-  digitalWrite(BUILTIN_LED, 0);
+  digitalWrite(LED_BUILTIN, 0);
 }
 
 inline void builtin_LED_off()
 {
-  digitalWrite(BUILTIN_LED, 1);
+  digitalWrite(LED_BUILTIN, 1);
 }
 
 inline void set_LCD_backlight(bool state, int brightness)
